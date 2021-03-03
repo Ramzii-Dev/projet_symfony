f39aa1b2f767f369e202a45121112e35d80e7588
Ramzii-Dev <ramzi446@hotmail.com>
2021-03-03 12:08:19 +0100

    03/03/2021

--- la_boutique/src/Controller/AccoutAddressController.php
+++ la_boutique/src/Controller/AccoutAddressController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Form\AddressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,8 +18,17 @@ class AccoutAddressController extends AbstractController
     }
 
     #[Route('/compte/ajouter-addresse', name: 'account_address_add')]
-    public function add(): Response
+    public function add(Request $request): Response
     {
-        return $this->render('account/address_add.html.twig');
+        $address = new Address();
+        $form = $this->createForm(AddressType::class,$address);
+        $form->handleRequest($request);
+        if($form->isSubmitted() &&  $form->isValid()){
+            $address->setUser($this->getUser());
+            dd($address);
+        }
+        return $this->render('account/address_add.html.twig',[
+            'form'=>$form->createView()
+        ]);
     }
 }
