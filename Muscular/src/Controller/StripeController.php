f88350098e13fda08ef70d7f0ef3b05ab2a3dec7
Ramzii-Dev <ramzi446@hotmail.com>
2021-03-10 18:13:10 +0100

    09/03/2021

---
+++ Muscular/src/Controller/StripeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\classe\Cart;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StripeController extends AbstractController
+{
+    #[Route('/commande/create-session', name: 'stripe_create-session')]
+    public function index(Cart $cart): Response
+    {
+        $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+        $product_for_stripe = [];
+
+        foreach ($cart->getFull() as $product){
+            $product_for_stripe[] =[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $product['product']->getPrice(),
+                    'product_data' => [
+                        'name' => $product['product']->getName(),
+                        'images' => [$YOUR_DOMAIN."/uploads/".$product['product']->getIllustration()],
+                    ],
+                ],
+                'quantity' =>$product['quantity'],
+            ];
+        }
+        Stripe::setApiKey('sk_test_51IT4M4BpsCG1okk4XgDJmDIVF8l2bQiHFtwtMrAz1zFoLfbz9Mu24bq7wCr8U3EqnOLSfhOwOJZHc6hCp0NIUSyn000ElKHvih');
+        $checkout_session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                $product_for_stripe
+            ],
+            'mode' => 'payment',
+            'success_url' => $YOUR_DOMAIN . '/success.html',
+            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+        ]);
+        $response = new JsonResponse(['id' => $checkout_session->id]);
+        return $response;
+
+    }
+}
